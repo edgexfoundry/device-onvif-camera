cdc4cddfe2b62df831bdafa8ce5260b4caf6e107
Brian Osburn <brian.j.osburn@intel.com>
2022-08-18 14:49:45 -0700

    test: adding credential test

--- internal/driver/credentials_test.go
+++ internal/driver/credentials_test.go
@@ -10,21 +10,11 @@ import (
 	"testing"
 
 	"github.com/IOTechSystems/onvif"
+	"github.com/edgexfoundry/go-mod-bootstrap/v2/bootstrap/interfaces/mocks"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-// TestTryGetCredentials_noAuth verifies when a credential is passed with a noAuth path, noAuth credentials are returned.
-func TestTryGetCredentials_noAuth(t *testing.T) {
-	driver, _ := createDriverWithMockService()
-	result, err := driver.tryGetCredentials(noAuthSecretPath)
-
-	expected := Credentials{
-		AuthMode: AuthModeNone,
-	}
-	require.NoError(t, err)
-	assert.Equal(t, expected, result)
-}
 
 // TestIsAuthModeValid verifies auth mode is set correctly.
 func TestIsAuthModeValid(t *testing.T) {
@@ -60,6 +50,79 @@ func TestIsAuthModeValid(t *testing.T) {
 		t.Run(test.input, func(t *testing.T) {
 			result := IsAuthModeValid(test.input)
 			assert.Equal(t, test.expected, result)
+		})
+	}
+}
+
+// TestTryGetCredentials verifies correct credentials are returned.
+func TestTryGetCredentials(t *testing.T) {
+
+	tests := []struct {
+		path          string
+		expected      Credentials
+		errorExpected bool
+		username      string
+		password      string
+		authMode      string
+	}{
+		{
+			path:     noAuthSecretPath,
+			username: "username",
+			password: "password",
+			authMode: onvif.DigestAuth,
+			expected: Credentials{
+				AuthMode: AuthModeNone,
+			},
+			errorExpected: false,
+		},
+		{
+			path:     "validPath",
+			username: "username",
+			password: "password",
+			authMode: onvif.DigestAuth,
+			expected: Credentials{
+				AuthMode: AuthModeDigest,
+				Username: "username",
+				Password: "password",
+			},
+			errorExpected: false,
+		},
+		{
+			path:          "invalidPath",
+			username:      "username",
+			password:      "password",
+			authMode:      onvif.DigestAuth,
+			expected:      Credentials{},
+			errorExpected: true,
+		},
+	}
+
+	driver, mockService := createDriverWithMockService()
+
+	mockSecretProvider := &mocks.SecretProvider{}
+
+	for i, _ := range tests {
+		if tests[i].errorExpected {
+			mockSecretProvider.On("GetSecret", tests[i].path, UsernameKey, PasswordKey, AuthModeKey).Return(nil, errors.NewCommonEdgeX(errors.KindServerError, "unit test error", nil)).Once()
+
+		} else {
+			mockSecretProvider.On("GetSecret", tests[i].path, UsernameKey, PasswordKey, AuthModeKey).Return(map[string]string{"username": tests[i].username, "password": tests[i].password, "mode": tests[i].authMode}, nil).Once()
+		}
+	}
+
+	mockService.On("GetSecretProvider").Return(mockSecretProvider)
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.path, func(t *testing.T) {
+			actual, err := driver.tryGetCredentials(test.path)
+
+			if test.errorExpected {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			assert.Equal(t, test.expected, actual)
 		})
 	}
 }
