e03c6e991c633265ca57616196638541aa716f8b
Mengyi Wang <mengyi.wang@canonical.com>
2023-03-22 18:07:28 +0100

    fix(snap): Refactor to avoid conflicts with readonly config provider directory (#279)
    Signed-off-by: Mengyi Wang <mengyi.wang@canonical.com>

--- snap/local/bin/source-env-file.sh
+++ snap/local/bin/source-env-file.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 # convert cmdline to string array
 ARGV=($@)
@@ -8,14 +8,14 @@ BINPATH="${ARGV[0]}"
 
 # binary name == service name/key
 SERVICE=$(basename "$BINPATH")
-ENV_FILE="$SNAP_DATA/config/$SERVICE/res/$SERVICE.env"
+ENV_FILE="$SNAP_DATA/config/$SERVICE/overrides.env"
 TAG="edgex-$SERVICE."$(basename "$0")
 
 if [ -f "$ENV_FILE" ]; then
     logger --tag=$TAG "sourcing $ENV_FILE"
     set -o allexport
     source "$ENV_FILE" set
-    set +o allexport 
+    set +o allexport
 fi
 
 exec "$@"
